97eee0549283fc6d9a5f2e2b2fdc15ea5b59fc97
Magno Efren <58579707+MagnoEfren@users.noreply.github.com>
2025-10-26 11:21:35 -0500

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,9 @@
+# core/__init__.py
+"""
+Módulo core: Contiene las clases base y gestores centrales
+"""
 from .base_widget import WidgetConfig
 from .widget_manager import WidgetManager
 
 __all__ = ['WidgetConfig', 'WidgetManager']
+
